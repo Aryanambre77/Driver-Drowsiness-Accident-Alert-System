599ae9f4185a465679b060460f8fa8845d382497
Aryanambre77 <159506189+Aryanambre77@users.noreply.github.com>
2024-04-27 21:07:54 +0530

    GPS code

---
+++ GPS.ino
@@ -0,0 +1,28 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial GPS(RXPin, TXPin);
+
+void setup(){
+  Serial.begin(9600);
+  GPS.begin(GPSBaud);
+}
+
+void loop(){
+  while (GPS.available() > 0){
+    gps.encode(GPS.read());
+    if (gps.location.isUpdated()){
+      Serial.print("Latitude= "); 
+      Serial.print(gps.location.lat(), 6);
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6);
+    }
+  }
+}
